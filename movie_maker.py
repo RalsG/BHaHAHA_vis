942ac97a9f96acc39e0fe5ba2f8e9f096bdbc2b5
RalsG <ralston.graves@gmail.com>
2025-03-05 19:22:03 -0800

    Bumped frame count to 1600 to account for extra pause frames at the end

--- movie_maker.py
+++ movie_maker.py
@@ -2,7 +2,7 @@ import imageio.v2 as imageio
 import os
 
 images = []
-for i in range(1540):
+for i in range(1600):
     filename = f"frames2/frame_{i:04d}.png"
     if not os.path.exists(filename):
         continue
